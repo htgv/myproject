c3c7faed94831d5bd15a017487242d47087d34cc
99 <1473023326@qq.com>
2017-08-11 15:13:48 +0800

    享受孤独

--- 99.php
+++ 99.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>flower</title>
-<meta name="description" content="">
-<meta name="keywords" content="">
-<link rel="stylesheet" href="css/bootstrap.min.css" />
-<link href="css/entry.css" rel="stylesheet">
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-</head>
-<body>
- <h1>99.php</h1>
-</body>
-</html>
+享受孤独
